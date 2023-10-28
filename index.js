9fc08947ba235b9a87240fe65eda8e2fddae7dd9
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2023-10-28 17:43:50 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// 1) Remove the DOM node 'main#main'
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// 2) Create an <h1> element and store it in the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// 3) Set the id of the 'newHeader' element to 'victory'
+newHeader.id = 'victory';
+
+// 4) Set the text of the 'newHeader' element to include "YOUR-NAME is the champion"
+const yourName = 'Your Name'; // Replace 'Your Name' with your actual name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append the 'newHeader' to the body of the document
+document.body.appendChild(newHeader);
